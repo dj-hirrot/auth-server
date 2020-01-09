7f7ad8e17684376d21f76986adc1a36a5f8cbff5
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-09 07:30:09 +0000

    improve test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,11 +6,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get join_path
 
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 end
