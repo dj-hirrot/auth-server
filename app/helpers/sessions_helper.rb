837a9544a81002fd92e9400595068a959b81290d
--global <hirotoshibutani@gmail.com>
2020-01-14 11:58:36 +0000

    added session method to helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,15 @@
 module SessionsHelper
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
   def login(user)
     session[:user_id] = user.id
+  end
+
+  def loggedin?
+    !current_user.nil?
   end
 end
