d0de2403ba02850311dd8cdf7ff507557aecc0b9
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-14 09:27:50 +0000

    display danger flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && authenticate(params[:session][:password])
       # something precess...
     else
+      flash.now[:danger] = 'invalid email/password combination'
       render :new
     end
   end
