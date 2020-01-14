9e886eadce4d995bd0381c2fc2ead5458badf39f
--global <hirotoshibutani@gmail.com>
2020-01-14 15:47:54 +0000

    user enable to remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       login user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
