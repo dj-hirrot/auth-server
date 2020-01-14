dc6d784b0346aea69eb819f4de3a6b3aeede9f3f
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-14 09:47:22 +0000

    add login process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
 
-    if user && authenticate(params[:session][:password])
-      # something precess...
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render :new
