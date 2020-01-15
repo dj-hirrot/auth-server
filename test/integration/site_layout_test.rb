662a838068499dbbd4ade6c3b0a3c6dcfa6cd367
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-15 09:40:03 +0000

    added test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users('dj-hirrot')
+  end
+
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
@@ -8,5 +12,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    get users_path
+    assert_redirected_to login_path
+    follow_redirect!
+    assert_select "div.alert-danger", "PLEASE LOGIN"
   end
 end
