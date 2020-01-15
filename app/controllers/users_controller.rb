236dbb6c3c1de6ed80a0b4285338394834934040
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-15 02:40:05 +0000

    resolved deprecate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    if @user.update_attributes(user_params)
+    if @user.update(user_params)
       # Process of updated success
     else
       render :edit
