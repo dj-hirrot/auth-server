00a43a6a514333224255500cc44246aeef6fc842
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-15 02:26:14 +0000

    added update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      # Process of updated success
+    else
+      render :edit
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
