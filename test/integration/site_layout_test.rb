7d08ffd412a7dfb622038320803d8735de3c5247
--global <hirotoshibutani@gmail.com>
2020-01-15 16:16:42 +0000

    added test of after login header links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -18,4 +18,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "div.alert-danger", "PLEASE LOGIN"
   end
+
+  test "layout links after login" do
+    login_as(@user)
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[data-toggle=?]", "dropdown"
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    get users_path
+    assert_select "h1", "All users"
+  end
 end
