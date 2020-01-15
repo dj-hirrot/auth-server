16b9b6c53d9ad70ee2d409e86695ee7a7753194d
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-15 07:11:54 +0000

    added test index page redirect if not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users('dj-hirrot')
+    @other_user = users('dj-yuta')
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get join_url
     assert_response :success
