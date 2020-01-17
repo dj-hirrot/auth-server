33025d7bf9fcaace4ca73c895e58db0a49ed190c
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-17 05:43:05 +0000

    create user activation method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+
+    if user && !user.activated? && user.authenticated?(:activation, user.activation_token)
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      login user
+      flash[:success] = "ACCOUNT ACTIVATED!!"
+      redirect_to user
+    else
+      flash[:danger] = "INVALID ACTIVATION LINK"
+      redirect_to root_url
+    end
+  end
 end
