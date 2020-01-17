9a7dcf741121d66456b3437012c7feeb2975b796
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-17 02:42:50 +0000

    added activation property

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,21 @@ User.create!(
   email: 'example@sample.org',
   password: 'foobar',
   password_confirmation: 'foobar',
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@sample.org"
   password = 'password'
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
+  )
 end
