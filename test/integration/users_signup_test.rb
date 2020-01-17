6f70764bd2c1be276749d60ce2e4f0995e14f521
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-17 08:32:06 +0000

    create test for account activate

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base::deliveries.clear
+  end
 
   test "invalid signup information" do
     get join_path
@@ -20,7 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get join_path
     assert_difference 'User.count', 1 do
       post join_path, params: {
@@ -33,8 +36,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
 
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    login_as user
+    assert_not is_loggedin?
+
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_loggedin?
+
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
     follow_redirect!
-    # assert_template 'users/show'
-    # assert is_loggedin?
+    assert_template 'users/show'
+    assert is_loggedin?
   end
 end
