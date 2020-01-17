42755422fa832db18fd1976155261fc610720cc9
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-17 09:57:11 +0000

    create method of send email for password reset

--- app/controllers/passwrod_resets_controller.rb
+++ app/controllers/passwrod_resets_controller.rb
@@ -2,6 +2,20 @@ class PasswrodResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = 'EMAIL SENT WITH PASSWORD RESET INSTRUCTIONS'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'EMAIL ADDRESS NOT FOUND'
+      render :new
+    end
+  end
+
   def edit
   end
 end
