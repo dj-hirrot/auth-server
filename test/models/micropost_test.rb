09f2d37635e2ef07f697ddaf962d969ec6c6825d
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-20 06:42:13 +0000

    added test of micropost model

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users('dj-hirrot')
+    @micropost = Micropost.new(content: 'Micropost', user_id: @user.id)
+  end
+
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user_id should be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
