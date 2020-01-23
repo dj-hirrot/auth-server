c5d109652c3b090d38d71beaf551ad120fee0f6b
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-23 10:06:23 +0000

    added test of profile page

--- test/integration/users_show_test.rb
+++ test/integration/users_show_test.rb
@@ -5,9 +5,20 @@ class UsersShowTest < ActionDispatch::IntegrationTest
     @user = users('dj-hirrot')
   end
 
-  test 'should redirect root_url non-activated user' do
+  test 'profile display' do
     get user_path(@user)
     assert_template 'users/show'
+    assert_select 'h1', text: @user.name
+    assert_select 'h1 > img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+
+  test 'should redirect root_url non-activated user' do
+    get user_path(@user)
     @user.update_attribute(:activated, false)
     @user.reload
     get user_path(@user)
