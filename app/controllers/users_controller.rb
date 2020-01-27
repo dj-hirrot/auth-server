a8ca176dc151d01d184ad6782b1dcf6d46b7e0cb
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-27 02:57:24 +0000

    move signin check method to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,14 +55,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def loggedin_user
-      unless loggedin?
-        store_location
-        flash[:danger] = 'PLEASE LOGIN'
-        redirect_to login_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
