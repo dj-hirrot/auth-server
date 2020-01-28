3d0ec2b94c63e01725617c33492a523b1ed95dd0
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-28 22:13:14 +0900

    added: validataion of picture size

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,12 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :picture_size
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, 'should be less than 5MB')
+      end
+    end
 end
